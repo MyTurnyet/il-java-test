c5e3c2e9f840cba4cf882575b7551421bb30723c
Paige Watson <paige.watson@outlook.com>
2021-04-10 12:16:05 -0700

    F Add ShoppingCart

---
+++ src/main/java/dev/paigewatson/models/ShoppingCart.java
@@ -0,0 +1,9 @@
+package dev.paigewatson.models;
+
+public class ShoppingCart
+{
+    public int itemCount()
+    {
+        return 0;
+    }
+}
---
+++ src/test/java/dev/paigewatson/models/ShoppingCartTests.java
@@ -0,0 +1,23 @@
+package dev.paigewatson.models;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Tag("Unit")
+public class ShoppingCartTests
+{
+    @Test
+    public void should_haveNoItems()
+    {
+        //assign
+        final ShoppingCart shoppingCart = new ShoppingCart();
+
+        //act
+        final int itemCount = shoppingCart.itemCount();
+        //assert
+        assertThat(itemCount).isEqualTo(0);
+    }
+}
