14cd281a2633dac3457eb6e85051ff0a81fac4b6
Paige Watson <paige.watson@outlook.com>
2021-04-11 11:14:33 -0700

    F check validity for endDate

--- src/main/java/dev/paigewatson/models/Discount.java
+++ src/main/java/dev/paigewatson/models/Discount.java
@@ -7,15 +7,20 @@ public class Discount
 {
     private final String requiredItemName;
     private final int requireNumber;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
 
     public Discount(String requiredItemName, int requireNumber, LocalDate startDate, LocalDate endDate, DiscountRule discountRule)
     {
         this.requiredItemName = requiredItemName;
         this.requireNumber = requireNumber;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
-    public boolean isValid(List<StoreItem> storeItems)
+    public boolean isValid(LocalDate saleDate, List<StoreItem> storeItems)
     {
+        if(endDate.isBefore(saleDate)) return false;
         final long matchingItemsCount = storeItems.stream().filter(storeItem -> storeItem.hasSameName(requiredItemName)).count();
         return matchingItemsCount >= requireNumber;
     }
--- src/test/java/dev/paigewatson/models/DiscountTests.java
+++ src/test/java/dev/paigewatson/models/DiscountTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static dev.paigewatson.models.Item.APPLE_NAME;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-    @Tag("Unit")
+@Tag("Unit")
 public class DiscountTests
 {
     @Test
@@ -18,46 +18,65 @@ public class DiscountTests
     {
         //assign
         final LocalDate today = LocalDate.now();
-        final DiscountRule discountRule = new DiscountRule( 50, APPLE_NAME);
+        final DiscountRule discountRule = new DiscountRule(50, APPLE_NAME);
         final Discount discount = new Discount(APPLE_NAME, 1, today, today, discountRule);
 
         final List<StoreItem> storeItems = new ArrayList<>();
         storeItems.add(Item.Apple());
 
         //act
-        final boolean canBeApplied = discount.isValid(storeItems);
+        final boolean canBeApplied = discount.isValid(today, storeItems);
         //assert
         assertThat(canBeApplied).isTrue();
     }
+
     @Test
     public void shouldNot_applyDiscount_whenRequiredAmount_isLessThanActualCount()
     {
         //assign
         final LocalDate today = LocalDate.now();
-        final DiscountRule discountRule = new DiscountRule( 50, APPLE_NAME);
+        final DiscountRule discountRule = new DiscountRule(50, APPLE_NAME);
         final Discount discount = new Discount(APPLE_NAME, 2, LocalDate.now(), LocalDate.now(), discountRule);
 
         final List<StoreItem> storeItems = new ArrayList<>();
         storeItems.add(Item.Apple());
 
         //act
-        final boolean canBeApplied = discount.isValid(storeItems);
+        final boolean canBeApplied = discount.isValid(today, storeItems);
         //assert
         assertThat(canBeApplied).isFalse();
     }
+
     @Test
     public void shouldNot_applyDiscount_whenRequiredItems_dontExist()
     {
         //assign
         final LocalDate today = LocalDate.now();
-        final DiscountRule discountRule = new DiscountRule( 50, APPLE_NAME);
-        final Discount discount = new Discount(APPLE_NAME, 1,  today, today,discountRule);
+        final DiscountRule discountRule = new DiscountRule(50, APPLE_NAME);
+        final Discount discount = new Discount(APPLE_NAME, 1, today, today, discountRule);
 
         final List<StoreItem> storeItems = new ArrayList<>();
         storeItems.add(Item.Bread());
 
         //act
-        final boolean canBeApplied = discount.isValid(storeItems);
+        final boolean canBeApplied = discount.isValid(today, storeItems);
+        //assert
+        assertThat(canBeApplied).isFalse();
+    }
+
+    @Test
+    public void shouldNot_applyDiscount_whenSaleDate_isAfterEndDate()
+    {
+        //assign
+        final LocalDate today = LocalDate.now();
+        final DiscountRule discountRule = new DiscountRule(50, APPLE_NAME);
+        final Discount discount = new Discount(APPLE_NAME, 1, today.minusDays(1), today.minusDays(1), discountRule);
+
+        final List<StoreItem> storeItems = new ArrayList<>();
+        storeItems.add(Item.Apple());
+
+        //act
+        final boolean canBeApplied = discount.isValid(today, storeItems);
         //assert
         assertThat(canBeApplied).isFalse();
     }
