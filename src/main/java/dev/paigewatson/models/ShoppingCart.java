8a86caeb92f973cf106f386fabe8e4c5e94fb54c
Paige Watson <paige.watson@outlook.com>
2021-04-11 09:16:46 -0700

    F add check to see if purchase date matches given date

--- src/main/java/dev/paigewatson/models/ShoppingCart.java
+++ src/main/java/dev/paigewatson/models/ShoppingCart.java
@@ -1,10 +1,23 @@
 package dev.paigewatson.models;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ShoppingCart
 {
     private final ArrayList<StoreItem> itemsInCart = new ArrayList<>();
+    private final LocalDate purchaseDate;
+
+    public ShoppingCart()
+    {
+    this(LocalDate.now());
+    }
+
+    public ShoppingCart(LocalDate purchaseDate)
+    {
+
+        this.purchaseDate = purchaseDate;
+    }
 
     public void addItem(StoreItem item)
     {
@@ -19,5 +32,10 @@ public class ShoppingCart
             total = storeItem.AddCostToTotal(total);
         }
         return total;
+    }
+
+    public boolean purchaseDateMatches(LocalDate expectedDate)
+    {
+        return expectedDate.compareTo(purchaseDate)==0;
     }
 }
--- src/test/java/dev/paigewatson/models/ShoppingCartTests.java
+++ src/test/java/dev/paigewatson/models/ShoppingCartTests.java
@@ -3,11 +3,37 @@ package dev.paigewatson.models;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Tag("Unit")
 public class ShoppingCartTests
 {
+    @Test
+    public void should_matchPurchaseDate()
+    {
+        //assign
+        LocalDate today = LocalDate.now();
+        final ShoppingCart shoppingCart = new ShoppingCart(today);
+
+        //act
+        final boolean dateMatches = shoppingCart.purchaseDateMatches(LocalDate.now());
+        //assert
+        assertThat(dateMatches).isTrue();
+    }
+    @Test
+    public void should_notMatch_PurchaseDate()
+    {
+        //assign
+        LocalDate today = LocalDate.now();
+        final ShoppingCart shoppingCart = new ShoppingCart(today);
+
+        //act
+        final boolean dateMatches = shoppingCart.purchaseDateMatches(LocalDate.now().plusDays(1));
+        //assert
+        assertThat(dateMatches).isFalse();
+    }
 
     @Test
     public void should_calculateCosts_6Apples_1Milk()
