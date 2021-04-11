ac3b0f3049da438c0c2725904b98ddc9c00ec361
Paige Watson <paige.watson@outlook.com>
2021-04-11 15:34:30 -0700

    T add AcceptanceTests file

---
+++ src/test/java/dev/paigewatson/AcceptanceTests.java
@@ -0,0 +1,59 @@
+package dev.paigewatson;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Unit")
+public class AcceptanceTests
+{
+    /***************
+     * These are the four acceptance tests from the readme.md
+     ***************/
+
+    @Test
+    public void BasketWith_3TinsOfSoup_2LoavesOfBread_boughtToday_returns_3_15()
+    {
+        //assign
+
+
+        //act
+
+        //assert
+
+    }
+
+    @Test
+    public void BasketWith_6Apples_1BottleOfMilk_boughtToday_returns_1_90()
+    {
+        //assign
+
+
+        //act
+
+        //assert
+
+    }
+    @Test
+    public void BasketWith_6Apples_1BottleOfMilk_bought5DaysFromNow_returns_1_90()
+    {
+        //assign
+
+
+        //act
+
+        //assert
+
+    }
+    @Test
+    public void BasketWith_3Apples_2TinsOfSoup_1LoafOfBread_bought5DaysFromNow_returns_1_97()
+    {
+        //assign
+
+
+        //act
+
+        //assert
+
+    }
+
+}
