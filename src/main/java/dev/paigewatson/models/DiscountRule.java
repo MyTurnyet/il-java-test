ba36e9e6772ac292a5c795fa1e322329d421962e
Paige Watson <paige.watson@outlook.com>
2021-04-11 11:29:11 -0700

    R remove commented code

--- src/main/java/dev/paigewatson/models/DiscountRule.java
+++ src/main/java/dev/paigewatson/models/DiscountRule.java
@@ -16,11 +16,6 @@ public class DiscountRule
         return (fullCost * discountPercentage) / 100;
     }
 
-//    public boolean isInEffect(LocalDate effectiveDate)
-//    {
-//        return effectiveDate.compareTo(discountStartDate) >= 0 && effectiveDate.compareTo(discountEndDate) <= 0;
-//    }
-
     public boolean matchesItemName(String StoreItemName)
     {
         return StoreItemName.equals(discountItemName);
